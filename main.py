f9bb9380f454498310028039e874a43fbd300e1f
zhuravlev_work <855434@gmail.com>
2023-09-19 11:41:54 +0300

    added backup scheduler

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio
+
 from aiogram.utils import executor
 from config import dp, bot
 from handlers import register_mh
-from database import sqlite_db
+from database import sqlite_db, backup_db_script
 from keyboards.admin_kb import set_main_menu
 
 
@@ -10,8 +12,12 @@ async def on_startup(_):
     await set_main_menu(bot)
     sqlite_db.sql_start()
 
+    loop = asyncio.get_event_loop()
+    loop.create_task(backup_db_script.scheduled_function())
+
+
 register_mh.register_handlers_user(dp)
 register_mh.register_handlers_admin(dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling(dp,  skip_updates=True, on_startup=on_startup)
