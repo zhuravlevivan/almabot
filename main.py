86d0347902314fb827dbd75d01171f539e864c52
zhuravlevivan <32869387+zhuravlevivan@users.noreply.github.com>
2023-09-23 13:10:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_startup(_):
     await set_main_menu(bot)
     sqlite_db.sql_start()
 
-    loop = asyncio.get_event_loop()
+    # loop = asyncio.get_event_loop()
     loop.create_task(backup_db_script.scheduled_function())
 
 
@@ -20,4 +20,7 @@ register_mh.register_handlers_user(dp)
 register_mh.register_handlers_admin(dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dp,  skip_updates=True, on_startup=on_startup)
+    # executor.start_polling(dp,  skip_updates=True, on_startup=on_startup)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(on_startup(None))
+    executor.start_polling(dp,  skip_updates=True)
